27d946dade8c7395674cb761fd0f623fadf46575
Shantanoo-HBWSL <shantanoo.chandorkar@hbwsl.com>
2024-12-05 00:13:28 +0530

    Removed duplicated code

--- src/blocks/accordion/index.php
+++ src/blocks/accordion/index.php
@@ -21,18 +21,6 @@ function responsive_block_editor_addons_accordion_add_frontend_assets() {
 			RESPONSIVE_BLOCK_EDITOR_ADDONS_VER,
 			true
 		);
-	} else if (!empty($widget_blocks)) {
-		foreach ($widget_blocks as $widget) {
-			if (!empty($widget['content'])) {
-				wp_enqueue_script(
-					'responsive_block_editor_addons-accordion-front-script',
-					plugins_url( 'accordion.js', __FILE__ ),
-					array(),
-					RESPONSIVE_BLOCK_EDITOR_ADDONS_VER,
-					true
-				);
-			}
-		}
 	}
 }
 
