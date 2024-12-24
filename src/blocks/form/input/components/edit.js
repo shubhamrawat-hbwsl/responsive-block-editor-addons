70598d9dca8e6544bce70f82adb24454b516b70c
Shantanoo-HBWSL <shantanoo.chandorkar@hbwsl.com>
2024-12-24 10:14:24 +0530

    TASK/WP-6726 and TASK/WP-6679

--- src/blocks/form/input/components/edit.js
+++ src/blocks/form/input/components/edit.js
@@ -89,6 +89,7 @@ export default class Edit extends Component {
     }
 
     return [
+      <style id={`responsive-block-editor-addons-form-input-style-${this.props.clientId}-inner`}>{EditorStyles(this.props)}</style>,
       <BlockControls key="controls">
       </BlockControls>,
       // Show the block controls on focus
